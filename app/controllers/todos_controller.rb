97f56494847325d8c513d1129c99af02f693b1b0
hyperbola-bear <hung.hin.wang@gmail.com>
2021-05-24 16:46:08 +0800

    removed redundancy(refactored)

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,5 @@
 class TodosController < ApplicationController
+    before_action :set_todo, only: [:edit, :show, :update, :destroy]
     def new
         @todo = Todo.new
     end
@@ -15,16 +16,16 @@ class TodosController < ApplicationController
     end
 
     def show
-        @todo = Todo.find(params[:id])
+        
     end
 
     def edit
-        @todo = Todo.find(params[:id])
+        
 
     end
 
     def update
-        @todo = Todo.find(params[:id])
+        
         if @todo.update(todo_params)
             flash[:notice] = "Todo was successfully updated"
             redirect_to todo_path(@todo)
@@ -38,7 +39,7 @@ class TodosController < ApplicationController
     end
 
     def destroy
-        @todo = Todo.find(params[:id])
+        
         @todo.destroy
         flash[:notice] = "Todo deleted successfully"
         redirect_to todos_path
@@ -54,5 +55,9 @@ class TodosController < ApplicationController
 
         def todo_params
             params.require(:todo).permit(:name, :description)
+        end
+        
+        def set_todo
+            @todo = Todo.find(params[:id])
         end
 end
